d64b946d53250af248bed7c55c3467ddea6b49d4
Anket Kohak <anketkohak40@gmail.com>
2024-08-20 15:14:50 +0530

    Accumulating Numbers in the Calculator 303

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    var isFinishTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
@@ -22,9 +22,17 @@ class ViewController: UIViewController {
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
+       
+        if let numValue = sender.currentTitle {
+            
+            if isFinishTypingNumber{
+                displayLabel.text = numValue
+                isFinishTypingNumber = false
+            }
+            else{
+                displayLabel.text = displayLabel.text!+numValue
+            }
+        }
     }
 
 }
